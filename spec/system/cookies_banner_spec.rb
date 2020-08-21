49143f6757a3e2ce8511b7d78b3a514baa22ab1b
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2020-08-21 14:07:23 +0200

    refactor

--- spec/system/cookies_banner_spec.rb
+++ spec/system/cookies_banner_spec.rb
@@ -38,37 +38,76 @@ describe "Cookies", type: :system do
     visit decidim.root_path
   end
 
-  it "shows the cookie banner" do
-    expect(page).to have_content "Accept"
-  end
-
-  it "shows the learn more button" do
-    expect(page).to have_content "Learn more"
-  end
-
-  it "doesn't show the revoke acceptance link" do
-    expect(page).not_to have_content "Revoke cookies acceptance"
-  end
-
-  context "when user accept the cookie policy" do
-    it_behaves_like "on decline or accept cookies policy", "Accept"
-    it_behaves_like "after refresh or navigate"
-  end
-
-  context "when user decline the cookie policy" do
-    it_behaves_like "on decline or accept cookies policy", "Decline"
-    it_behaves_like "after refresh or navigate"
-  end
-
-  context "when user clicks on 'learn more' button" do
+  context "when no other cookies are defined" do
     before do
       click_button "Learn more"
     end
 
-    it "opens a new modal" do
+    it "shows the default cookies" do
+      expect(Rails.application.config).not_to respond_to(:cookies)
+
       within ".orejime-ModalWrapper" do
-        expect(page).to have_content "Information that we collect"
-        expect(page).to have_content "privacy policy"
+        expect(page).to have_content "decidim-cc"
+        expect(page).to have_content "decidim_session"
+        expect(page).not_to have_content "Matomo"
+      end
+    end
+  end
+
+  describe "with cookies example defined" do
+    before do
+      Rails.application.config.cookies = [
+          {
+              name: "matomo",
+              title: "Matomo",
+              cookies: %w(matomo_session pk_id pk_ses _pk_ref _pk_cvar),
+              purposes: %w(tracking analytics)
+          }
+      ]
+    end
+
+    it "shows the cookie banner" do
+      expect(page).to have_content "Accept"
+    end
+
+    it "shows the learn more button" do
+      expect(page).to have_content "Learn more"
+    end
+
+    it "doesn't show the revoke acceptance link" do
+      expect(page).not_to have_content "Revoke cookies acceptance"
+    end
+
+    context "when user accept the cookie policy" do
+      it_behaves_like "on decline or accept cookies policy", "Accept"
+      it_behaves_like "after refresh or navigate"
+    end
+
+    context "when user decline the cookie policy" do
+      it_behaves_like "on decline or accept cookies policy", "Decline"
+      it_behaves_like "after refresh or navigate"
+    end
+
+    context "when user clicks on 'learn more' button" do
+      before do
+        click_button "Learn more"
+      end
+
+      it "opens a new modal" do
+        within ".orejime-ModalWrapper" do
+          expect(page).to have_content "Information that we collect"
+          expect(page).to have_content "privacy policy"
+        end
+      end
+
+      it "shows all cookies" do
+        expect(Rails.application.config).to respond_to(:cookies)
+
+        within ".orejime-ModalWrapper" do
+          expect(page).to have_content "decidim-cc"
+          expect(page).to have_content "decidim_session"
+          expect(page).to have_content "Matomo"
+        end
       end
     end
   end
